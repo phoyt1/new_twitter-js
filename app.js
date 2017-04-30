e618df0dc3dd12cc3bbb5dd3de8bc304f7ee0520
Patrick Hoyt <ptrckhoyt120@gmail.com>
2017-04-30 15:47:15 -0400

    basic template

--- app.js
+++ app.js
@@ -1,13 +1,33 @@
 const express = require('express');
 const morgan = require('morgan');
+const nunjucks = require('nunjucks');
+const routes = require('./routes');
 const app = express();
 
+app.set('view engine', 'html'); // have res.render work with html files
+app.engine('html', nunjucks.render); // when giving html files to res.render, tell it to use nunjucks
+nunjucks.configure('views',{ noCache: true }); // point nunjucks to the proper directory for templates
 app.listen(3000,function(req,res){
   console.log('Listening on port 3000');
 });
 
 app.use(morgan('dev'));
+app.use(express.static('public'));
+app.use('/',routes);
+// app.get('/',function(req,res){
+//   res.render('index.html',people);
+//   //res.send('Welcome to my lovely homepage :)');
 
-app.get('/',function(req,res){
-  res.send('Welcome to my lovely homepage :)');
-});
+// });
+
+
+// var people = {
+//   title: 'An Example',
+//   people: [
+//     {name:'pat'},
+//     {name:'bob'},
+//     {name:'shon'}
+//   ]
+// }
+
+
